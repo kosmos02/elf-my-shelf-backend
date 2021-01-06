5c8ef01b70b4bb300da806f6568669d8ea8f62e0
kosmos02 <alexandrgabe@gmail.com>
2021-01-06 09:25:13 -0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 Model.create(name: "Candy Cane", gltf: "assets/Candy_cane/275_Candy Cane.gltf", usdz: "assets/Candy_cane/275_Candy Cane.usdz", author: "Google")
 Model.create(name: "Christmas Tree", gltf: "assets/Christmas_tree/model.gltf", usdz: "assets/Christmas_tree/model.usdz", author: "Alex Safayan")
 Model.create(name: "Snow Man", gltf: "assets/Frosty/model.gltf", usdz: "assets/Frosty/model.usdz", author: "Alex Safayan")
@@ -13,4 +14,4 @@ Model.create(name: "Presents", gltf: "assets/Presents/model.gltf", usdz: "assets
 Model.create(name: "Santa Hat", gltf: "assets/Santa Hat/model.gltf", usdz: "assets/Santa Hat/model.usdz", author: "Joe Dorman")
 Model.create(name: "Stocking", gltf: "assets/Stocking/model.gltf", usdz: "assets/Stocking/model.usdz", author: "Olga Nabatova")
 Model.create(name: "Tree Ornament", gltf: "assets/Tree_ornament/model.gltf", usdz: "assets/Tree_ornament/model.usdz", author: "Olga Nabatova")
-Model.create(name: "Wreath", gltf: "assets/Wreath/model.gltf", usdz: "front-end/assets/Wreath/model.usdz", author: "Olga Nabatova")
+Model.create(name: "Wreath", gltf: "assets/Wreath/model.gltf", usdz: "assets/Wreath/model.usdz", author: "Olga Nabatova")
